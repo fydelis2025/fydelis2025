218ec661d886d683d44c75df8cdc48f24b591b9f
Adiel Santos Fontes <adiel.vereador@gmail.com>
2026-06-16 14:05:54 -0300

    Add scroll reveal effect for specified elements

--- js/scroll.js
+++ js/scroll.js
@@ -1,1 +1,31 @@
+const elements =
+document.querySelectorAll(
+'.card,.projeto,.grid div'
+);
 
+function reveal(){
+
+elements.forEach(el=>{
+
+const windowHeight =
+window.innerHeight;
+
+const top =
+el.getBoundingClientRect().top;
+
+if(top < windowHeight - 100){
+
+    el.classList.add('show');
+
+}
+
+});
+
+}
+
+window.addEventListener(
+'scroll',
+reveal
+);
+
+reveal();
