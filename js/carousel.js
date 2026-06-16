c9e698a3f1cd59c7101f25e685601a7edede1672
Adiel Santos Fontes <adiel.vereador@gmail.com>
2026-06-16 14:05:04 -0300

    Implement automatic slide transition for carousel

--- js/carousel.js
+++ js/carousel.js
@@ -1,1 +1,18 @@
+const slides = document.querySelector('.slides');
 
+let current = 0;
+
+function nextSlide(){
+
+    current++;
+
+    if(current > 2){
+        current = 0;
+    }
+
+    slides.style.transform =
+    `translateX(-${current * 33.333}%)`;
+
+}
+
+setInterval(nextSlide,5000);
